a9f69340479d854ad81796889635fca23629416d
Spook <spook.devv@gmail.com>
2024-08-13 20:55:09 -0400

    Initial Landing page commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,27 @@
+import { Inter } from 'next/font/google'
+import clsx from 'clsx'
+
+import '@/styles/tailwind.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+export const metadata = {
+  title: {
+    template: '%s - Trail Twin',
+    default: 'Trail Twin - Find a ride on your own time.',
+  },
+  description:
+    'Find time to ride with a partner on your time. Meet new riding buddies and make new friendships!',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={clsx('bg-stone-50 antialiased', inter.variable)}>
+      <body>{children}</body>
+    </html>
+  )
+}
