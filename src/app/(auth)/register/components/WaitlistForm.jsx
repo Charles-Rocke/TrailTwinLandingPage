3a1821d183b53ede6d92ffc04a7229076825c9a7
Spook <spook.devv@gmail.com>
2024-08-16 18:01:14 -0400

    Integrated backend for waitlist form

---
+++ src/app/(auth)/register/components/WaitlistForm.jsx
@@ -0,0 +1,102 @@
+// Make sure to run npm install @formspree/react
+// For more help visit https://formspr.ee/react-help
+'use client'
+import React from 'react'
+import { useForm, ValidationError } from '@formspree/react'
+import { Button } from '@/components/Button'
+import { SelectField, TextField } from '@/components/Fields'
+
+export function WaitlistForm() {
+  const [state, handleSubmit] = useForm('meojzvjd')
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>
+  }
+  return (
+    <form
+      onSubmit={handleSubmit}
+      action="https://formspree.io/f/meojzvjd"
+      method="POST"
+    >
+      <div className="grid grid-cols-2 gap-6">
+        <TextField
+          id="firstName"
+          label="First name"
+          name="firstName"
+          type="text"
+          autoComplete="given-name"
+          required
+        />
+        <ValidationError
+          prefix="firstName"
+          field="firstName"
+          errors={state.errors}
+        />
+        <TextField
+          id="lastName"
+          label="Last name"
+          name="last_name"
+          type="text"
+          autoComplete="family-name"
+          required
+        />
+        <ValidationError
+          prefix="lastName"
+          field="lastName"
+          errors={state.errors}
+        />
+        <TextField
+          id="email"
+          className="col-span-full"
+          label="Email address"
+          name="email"
+          type="email"
+          autoComplete="email"
+          required
+        />
+        <ValidationError prefix="email" field="email" errors={state.errors} />
+        {/* <TextField
+            className="col-span-full"
+            label="Password"
+            name="password"
+            type="password"
+            autoComplete="new-password"
+            required
+          /> */}
+        <SelectField
+          id="referralSource"
+          className="col-span-full"
+          label="How did you hear about us?"
+          name="referralSource"
+        >
+          <option>Instagram</option>
+          <option>Facebook</option>
+          <option>Reddit</option>
+          <option>Google Search</option>
+          <option>Other</option>
+        </SelectField>
+        <ValidationError
+          prefix="referralSource"
+          field="referralSource"
+          errors={state.errors}
+        />
+      </div>
+
+      {/* <label htmlFor="email">Email Address</label>
+      <input id="email" type="email" name="email" />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <textarea id="message" name="message" />
+      <ValidationError prefix="Message" field="message" errors={state.errors} /> */}
+      {/* <button type="submit" disabled={state.submitting}>
+        Submit
+      </button> */}
+      <Button
+        type="submit"
+        color="green"
+        className="mt-8 w-full"
+        disabled={state.submitting}
+      >
+        Submit
+      </Button>
+    </form>
+  )
+}
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { AuthLayout } from '@/components/AuthLayout'
 import { Button } from '@/components/Button'
 import { SelectField, TextField } from '@/components/Fields'
+import { WaitlistForm } from './components/WaitlistForm'
 
 export const metadata = {
   title: 'Sign Up',
@@ -25,54 +26,7 @@ export default function Register() {
         <>Sign up to secure your spot on the waitlist and we’ll be in touch!</>
       }
     >
-      <form>
-        <div className="grid grid-cols-2 gap-6">
-          <TextField
-            label="First name"
-            name="first_name"
-            type="text"
-            autoComplete="given-name"
-            required
-          />
-          <TextField
-            label="Last name"
-            name="last_name"
-            type="text"
-            autoComplete="family-name"
-            required
-          />
-          <TextField
-            className="col-span-full"
-            label="Email address"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-          />
-          {/* <TextField
-            className="col-span-full"
-            label="Password"
-            name="password"
-            type="password"
-            autoComplete="new-password"
-            required
-          /> */}
-          <SelectField
-            className="col-span-full"
-            label="How did you hear about us?"
-            name="referral_source"
-          >
-            <option>Instagram</option>
-            <option>Facebook</option>
-            <option>Reddit</option>
-            <option>Google Search</option>
-            <option>Other</option>
-          </SelectField>
-        </div>
-        <Button type="submit" color="green" className="mt-8 w-full">
-          Submit
-        </Button>
-      </form>
+      <WaitlistForm />
     </AuthLayout>
   )
 }
